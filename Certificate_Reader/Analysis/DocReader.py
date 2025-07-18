e4789d772df6ece002f3d3b3cc9f1467f27b130c
Alisa <alisa.kulonpaa@gmail.com>
2025-07-19 00:26:25 +0300

    Now images are processed in pdf and docx text

--- Certificate_Reader/Analysis/DocReader.py
+++ Certificate_Reader/Analysis/DocReader.py
@@ -4,21 +4,44 @@ from pdfminer.high_level import extract_text
 from PIL import Image
 import pytesseract
 import docx2txt as d2t
+import os
 
+pytesseract.pytesseract.tesseract_cmd = r'D:\ReaderStuff\tesseract.exe'
 
 #PDF
 def PdfText(pdf_path):
-    return extract_text(pdf_path)
+    text = extract_text(pdf_path)
+
+    #images in pdf
+    pdf = fitz.open(pdf_path)
+    for i in range(len(pdf)):
+        page = pdf[i]
+        images = page.get_images()
+        for image in images:
+            base_img = pdf.extract_image(image[0])
+            image_data = base_img["image"]
+            img = Image.open(io.BytesIO(image_data))
+            ocr_text = pytesseract.image_to_string(img)
+            text += "\n[Image OCR Text]:\n" + ocr_text
+    return text
 
 #IMG
 def ImgText(img_path):
-    pytesseract.pytesseract.tesseract_cmd = r'D:\ReaderStuff\tesseract.exe'
     img = Image.open(img_path)
     return pytesseract.image_to_string(img)
 
 #WORD
-def DocxText (path_to_file, image_folder=None, get_text=True):
+def DocxText (path_to_file, image_folder="temp_docx_images", get_text=True):
+    os.makedirs(image_folder, exist_ok=True)
     text = d2t.process(path_to_file, image_folder)
+
+    #images in docx
+    for filename in os.listdir(image_folder):
+        if filename.lower().endswith((".jpg", "jpeg", ".png")):
+            img_path = os.path.join(image_folder, filename)
+            ocr_text = ImgText(img_path)
+            text += f"\n[OCR from image {filename}]:\n{ocr_text}"
+
     return text if get_text else ""
 
 #TXT
--- Certificate_Reader/Analysis/analysis.py
+++ Certificate_Reader/Analysis/analysis.py
@@ -1,39 +1,40 @@
-from langchain_openai import ChatOpenAi
+from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
 import fitz
+from Identifier import AnalyzeFile
 
 
 #load API key from .env
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPEN_API_KEY")
 
-llm = ChatOpenAI(model"gpt-4o-mini", api_key=OPENAI_API_KEY)
+llm = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
 
-def read_pdf(filename):
-    with fitz.open(filename) as doc:
-        return "\n".join([page.get_text() for page in doc]) 
-    
-    text = read_pdf("example.pdf")
+#Test
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.normpath(os.path.join(current_dir, "..", "testdata", "docx1.docx"))
+    text = AnalyzeFile(filepath)
 
-    #Prompt
-    PROMPT_TEMPLATE = """
-    You are an assistant for question-answering tasks.
-    User gave the following job certificate:
+#Prompt
+PROMPT_TEMPLATE = """
+You are an assistant for question-answering tasks.
+User gave the following job certificate:
 
-    {context}
+{context}
 
-    ---
+---
 
-    Answer the following questions:
-    1. What were the employees duties?
-    2. When did the work take place?
-    3. In what company did they work at?
+Answer the following questions:
+1. What were the employees duties?
+2. When did the work take place?
+3. In what company did they work at?
 
-    Answer in a structured manner.
-    Use the given data to answer the question. If you don't know the answer, say you don't know.
-    Don't make up anything.
-    """
+Answer in a structured manner.
+Use the given data to answer the question. If you don't know the answer, say you don't know.
+Don't make up anything.
+"""
 
 prompt = PROMPT_TEMPLATE.format(context=text)
 response = llm.invoke(prompt)
